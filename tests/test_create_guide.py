cc3b0b70b39f3bf8cb2a7e2980bd2b04a47ec11c
alan <alan@cuenca.com>
2020-01-07 19:24:08 -0600

    Change tests

--- tests/test_create_guide.py
+++ tests/test_create_guide.py
@@ -1,11 +1,18 @@
 import pytest
 
 from dhlmex import Client, get_guides_data
+from dhlmex.exceptions import DhlmexException
 
 
 @pytest.mark.vcr
 def test_create_guide():
     client = Client()
     guides = get_guides_data(client)
-    assert guides
     client._logout()
+    print("guides from test")
+    print(guides)
+    if guides:
+        assert guides
+    else:
+        with pytest.raises(DhlmexException) as execinfo:
+            assert str(execinfo.value) == f"No available guides"
