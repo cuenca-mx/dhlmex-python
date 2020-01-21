4b38f153c8b0908ab171aba2d025f3b5574e02b6
alan <alan@cuenca.com>
2020-01-21 16:33:20 -0600

    Fix exception texts

--- tests/resources/test_invalid_data.py
+++ tests/resources/test_invalid_data.py
@@ -8,7 +8,7 @@ def test_invalid_destination(client, origin, invalid_destination, details):
 
     with pytest.raises(DhlmexException) as execinfo:
         client.guides.create_guide(origin, invalid_destination, details)
-        assert str(execinfo.value) == f'Invalid destiny postal code'
+        assert str(execinfo) == 'Error while creating guide'
 
 
 @pytest.mark.vcr
@@ -16,4 +16,4 @@ def test_invalid_postal_code(client, origin, invalid_postal_code, details):
 
     with pytest.raises(DhlmexException) as execinfo:
         client.guides.create_guide(origin, invalid_postal_code, details)
-        assert str(execinfo.value) == f'Invalid destiny postal code'
+        assert str(execinfo.value) == 'Invalid destiny postal code'
