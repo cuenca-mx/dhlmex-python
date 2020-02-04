4c1cf988bb33326fc96f7a2a26366e29839f7360
alan <alan@cuenca.com>
2020-02-04 15:46:02 -0600

    Some fixes

--- dhlmex/resources/base.py
+++ dhlmex/resources/base.py
@@ -2,6 +2,7 @@ import re
 from typing import ClassVar, Dict
 
 from bs4 import BeautifulSoup
+from dhlmex.exceptions import DhlmexException
 from requests import Response
 
 
@@ -39,15 +40,20 @@ class Resource:
         view_state = soup.find('input', id='javax.faces.ViewState').attrs[
             'value'
         ]
-        js = soup.find('a', text=action['text']).attrs['onclick']
-        matches = re.findall(r"\'(.+?)\'", js)
-        form_ids = [match for match in matches if match.startswith('j_id')]
-        j_pair_id = form_ids[1].split(',')[0]
-        j_id = form_ids[0]
+        a = soup.find('a', text=action['text'])
+        if a is None:
+            print(f'DEBUG: {resp.text}')
+            raise DhlmexException('Debug')
+        else:
+            js = soup.find('a', text=action['text']).attrs['onclick']
+            matches = re.findall(r"\'(.+?)\'", js)
+            form_ids = [match for match in matches if match.startswith('j_id')]
+            j_pair_id = form_ids[1].split(',')[0]
+            j_id = form_ids[0]
 
-        return {
-            j_id: j_id,
-            j_pair_id: action['code'],
-            'javax.faces.ViewState': view_state,
-            action['end']: action['end'],
-        }
+            return {
+                j_id: j_id,
+                j_pair_id: action['code'],
+                'javax.faces.ViewState': view_state,
+                action['end']: action['end'],
+            }
